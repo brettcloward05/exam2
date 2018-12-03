6d6ff5f4624c1400a2926b07bdae063481498791
Brett Cloward <brettcloward@mail.weber.edu>
2018-12-03 15:47:27 +0000

    second commit

--- task2.py
+++ task2.py
@@ -8,15 +8,39 @@ from __future__ import print_function
 def is_valid_password(password):
     """
     Checks to see if the new password is valid
+    :param: password
     """
+    print("Please re-enter your password: ")
+    password_two = input()
+    while(password != password_two):
+        print("That password didn't have the required properties. # password did not match")
+        main()
+    # The password must be at least 8 char long
+    length = len(password)
+    if length != 8:
+        print ("That password didn't have the required properties. # password length")
+        main()
+    # The password must have one lower case
+    if password.islower is True:
+        print ("That password didn't have the required properties. # No lower case letter")
+        main()
+    # The password must have one upper case
+    if password.isupper is True:
+        print("That password didn't have the required properties. # No upper case letter")
+        main()
+    #The password must have one digit
+    if password.isalpha is True:
+        print("that password didn't have the required properties.   # No digit")
+        main()
 
 
 def main():
     """
     Test your module
     """
-
-    pass
+    print("Enter your password: ")
+    password = input()
+    is_valid_password(password)
 
 
 if __name__ == "__main__":
---
+++ task5.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""
+Checks two files to see what is common among them
+"""
+from __future__ import print_function
+
+
+
+def main():
+    """
+    Test your module
+    """
+
+    pass
+
+
+if __name__ == "__main__":
+    main()
+    exit(0)
