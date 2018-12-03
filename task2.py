5aaba2d83215a1a14e5565e7d9e8505749411e93
Brett Cloward <brettcloward@mail.weber.edu>
2018-12-03 14:40:35 +0000

    initial commit

---
+++ task2.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+"""
+This program has a user enter in a password and runs a series of checks
+"""
+from __future__ import print_function
+
+
+def is_valid_password(password):
+    """
+    Checks to see if the new password is valid
+    """
+
+
+def main():
+    """
+    Test your module
+    """
+
+    pass
+
+
+if __name__ == "__main__":
+    main()
+    exit(0)
